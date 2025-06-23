96f5899295d0b001071333caf2e76870514374e9
Nischal079 <Nischal0987654@gmail.com>
2025-06-23 15:52:05 +0545

    practice unfinished

--- SQL files/Practicedb.sql
+++ SQL files/Practicedb.sql
@@ -1,7 +1,15 @@
 create database Order_new;
-create table emp (
-id int,
-Names varchar(30),
-Address varchar(50),
-Salary int
+use Order_new;
+create table emp (id int,Names varchar(30),Address varchar(50),Salary int)
+
+-- SQL Practice (Types of Constraints):
+-- Not Null         -- Foreign Key
+-- Unique.          -- Check
+-- Primary Key.     -- Default
+
+-- 1. Not Null and Unique:
+create table emp1 (
+    id int,
+    email varchar(128) Not Null,
+    phone varchar (10) Unique
 )
